81e9c16d4f02b235bb11d4e51921e799fd27e2d5
cgf <cgf_1020@163.com>
2024-11-28 09:42:02 +0800

    Initial commit

---
+++ python1.py
@@ -0,0 +1,15 @@
+import requests
+import re
+
+# C语言中的 main()函数 入口
+if __name__ == '__main__':
+    url = 'https://www.weather.com.cn/weather.html'
+    resp = requests.get(url)
+    resp.encoding = 'utf-8'
+    print(resp.text)
+
+
+
+
+
+
